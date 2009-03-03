cc3cdfce00c19332aae362e25d9619be9e8b333f
Jason May <jason.a.may@gmail.com>
2009-03-03 12:56:06 -0500

    add #jobs command to view a limited amount of job listings

---
+++ lib/Dataninja/Bot/Command/Jobs.pm
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+package Dataninja::Bot::Command::Jobs;
+use URI;
+use Web::Scraper;
+use base 'Dataninja::Bot::Command';
+
+=head1 DESCRIPTION
+
+Reminds a user to blah blah TODO
+
+=cut
+
+sub pattern { qr/^#jobs\b/ }
+
+sub run {
+my $craigslist = scraper {
+    process 'blockquote>p>a', 'titles[]' => 'TEXT';
+};
+
+my $query = Jifty->config->app("clquery");
+my $job_data = $craigslist->scrape(URI->new("http://york.craigslist.org/search/jjj?query=$query"));
+
+my @jobs = @{$job_data->{titles}};
+
+return join q{, } =>
+    map {
+        my @words = split ' ', $_;
+        $_ = join ' ', @words[0 .. ($#words > 4 ? 4 : $#words)];
+        chop while /[^\w\d]$/;
+#        warn $_;
+        "[$_]";
+    }
+    (@jobs[0 .. ($#jobs > 4 ? 4 : $#jobs)]);
+}
+
+sub usage { "#remind <nick|me> <description> (in|at) <when>" }
+sub current_user_can { 1 }
+
+1;
