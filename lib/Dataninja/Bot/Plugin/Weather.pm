f4d118f7fb7cd9ac6954231eccca1ff2075609bc
Jason May <jason.a.may@gmail.com>
2009-03-30 21:13:10 -0400

    weather plugin was quite broken; had to tear into it pretty deeply

--- lib/Dataninja/Bot/Plugin/Weather.pm
+++ lib/Dataninja/Bot/Plugin/Weather.pm
@@ -4,27 +4,27 @@ use Weather::Underground;
 use String::Util qw/crunch/;
 extends 'Dataninja::Bot::Plugin::Base';
 
-sub _get_weather  {
+sub get_weather  {
     my $place = shift;
     my $weather_data = Weather::Underground->new(place => $place)
         or return "$!";
     return $weather_data->get_weather;
 }
 
-sub _weather_output {
+sub weather_output {
     my $get_weather = shift;
     my $place = shift;
     my $target_weather = $get_weather->[0];
     return
-        sprintf(
-            'weather for "%s" - temp: %sF (%sC). humidity: %s%%. wind: %smph. conditions: %s.',
-            $place,
-            $target_weather->{temperature_fahrenheit},
-            $target_weather->{temperature_celsius},
-            $target_weather->{humidity},
-            $target_weather->{wind_milesperhour},
-            $target_weather->{conditions}
-        );
+    sprintf(
+        'weather for "%s" - temp: %sF (%sC). humidity: %s%%. wind: %smph. conditions: %s.',
+        $place,
+        $target_weather->{temperature_fahrenheit},
+        $target_weather->{temperature_celsius},
+        $target_weather->{humidity},
+        $target_weather->{wind_milesperhour},
+        $target_weather->{conditions}
+    );
 }
 
 around 'command_setup' => sub {
@@ -37,67 +37,34 @@ around 'command_setup' => sub {
         my ($weather_data, $get_weather);
 
         my $areas = Dataninja::Model::AreaCollection->new;
-        $areas->limit(column => 'nick', value => $self->nick);
-        if ($place) {
-            my $area;
+        $areas->limit(column => 'nick', value => ($place || $self->nick));
+        if ($areas->count > 0) {
+            my $area = $areas->next;
+            my $new_place = $area->location;
+            my $get_weather = get_weather($new_place);
+            return weather_output($get_weather, $new_place);
+        }
+        elsif (!$place) {
+            return "you have no location set!";
+        }
+
+        if ($get_weather = get_weather($place)) {
+            my $areas = Dataninja::Model::AreaCollection->new;
+            $areas->limit(column => 'nick', value => $place);
             if ($areas->count > 0) {
-                $area = $areas->next;
-                $area->set_location($place);
+                my $nick_area = $areas->next;
+                $nick_area->set_location($place);
             }
             else {
-                $area = Dataninja::Model::Area->new;
-                $area->create(
+                my $new_area = Dataninja::Model::Area->new;
+                my ($ok, $error) = $new_area->create(
+                    nick => $self->nick,
                     location => $place,
-                    nick     => $self->nick,
-                    network  => $self->network,
                 );
+                return $error unless $ok;
             }
-
-            unless ($get_weather = _get_weather($place)) {
-                my $weather_from_nick = Dataninja::Model::AreaCollection->new;
-                # $place is a nick in this case
-                $weather_from_nick->limit(column => 'nick', value => $place);
-                if ($weather_from_nick->count > 0) {
-                    my $nick_area = $weather_from_nick->next;
-                    return "Invalid area" unless $nick_area;
-                    my $place = $nick_area->location;
-                    $weather_data = Weather::Underground->new(place => $place);
-                    $get_weather = $weather_data->get_weather;
-                    return _weather_output($get_weather, $place) if $get_weather;
-                }
-                else {
-                    return "Invalid area";
-                }
-            }
+            return weather_output(get_weather($place), $place);
         }
-        else {
-            if ($areas->count > 0) {
-                my $area = $areas->next;
-                $place = $area->location;
-            }
-            return "I am so buggy! FIX ME" unless $place;
-        }
-
-        $get_weather = _get_weather($place);
-        unless ($get_weather) {
-            my $weather_from_nick = Dataninja::Model::AreaCollection->new;
-            # $place is a nick in this case
-            $weather_from_nick->limit(column => 'nick', value => $place);
-            if ($weather_from_nick) {
-                my $nick_area = $weather_from_nick->next;
-                return "Invalid area" unless $nick_area;
-                $place = $nick_area->location;
-                $get_weather = _get_weather($place);
-                return _weather_output($get_weather, $place) if $get_weather;
-            }
-            else {
-                return "Invalid area";
-            }
-        }
-
-        return "I am so buggy! FIX ME" unless $get_weather;
-
-        return _weather_output($get_weather, $place);
     };
 
     $self->command(weather => $weather_code);
