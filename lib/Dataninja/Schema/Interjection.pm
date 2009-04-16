2c4e9085b19958f04ff707b8726716074109c770
Jason May <jason.a.may@gmail.com>
2009-04-15 23:29:48 -0400

    fix table indication for Interjections

--- lib/Dataninja/Schema/Interjection.pm
+++ lib/Dataninja/Schema/Interjection.pm
@@ -4,7 +4,7 @@ use warnings;
 use base qw/DBIx::Class::Schema/;
 
 __PACKAGE__->load_components(qw/PK::Auto Core/);
-__PACKAGE__->table('areas');
+__PACKAGE__->table('interjections');
 __PACKAGE__->add_columns(qw/id message interjected network channel/);
 __PACKAGE__->set_primary_key(qw/id/);
 
