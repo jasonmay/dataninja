08fdd7756245564febb3aa8a6802672c5edc8801
Jason May <jason.a.may@gmail.com>
2009-04-19 18:34:21 -0400

    connect_with_defaults

--- lib/Dataninja/Schema.pm
+++ lib/Dataninja/Schema.pm
@@ -2,6 +2,18 @@ package Dataninja::Schema;
 use strict;
 use warnings;
 use base qw/DBIx::Class::Schema/;
+use Dataninja::Config;
+
+sub connect_with_defaults {
+    my $class = shift;
+
+    my $config = Dataninja::Config->new(@_);
+    my $schema = $class->connect(
+        'dbi:Pg:dbname=' . $config->main->{database}->{name},
+        $config->main->{database}->{user},
+        $config->main->{database}->{pass},
+    );
+}
 
 __PACKAGE__->load_classes;
 
