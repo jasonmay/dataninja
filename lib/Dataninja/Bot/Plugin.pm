e325ae6b463872c695c08a3e50a24320c443c1ca
Jason May <jason.a.may@gmail.com>
2009-05-03 23:03:31 -0400

    add a general Plugin package for doc purposes

---
+++ lib/Dataninja/Bot/Plugin.pm
@@ -0,0 +1,47 @@
+package Dataninja::Bot::Plugin;
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+Dataninja plugins use Module::Pluggable as the backend. The base is
+L<Dataninja::Bot::Plugin::Base>. If you want to learn how to create a module
+from scratch, see the documentation in that module.
+
+=head1 CURRENT PLUGINS
+
+=item * Base
+
+=item * Botsnack
+
+=item * CalcRelay
+
+=item * Colors
+
+=item * Daysuntil
+
+=item * Echo
+
+=item * Jobs
+
+=item * Last
+
+=item * Remind
+
+=item * Seen
+
+=item * Task
+
+=item * Translating
+
+=item * Twentyfour
+
+=item * Twitter
+
+=item * Unit
+
+=item * Weather
+
+=item * Weeksuntil
+
