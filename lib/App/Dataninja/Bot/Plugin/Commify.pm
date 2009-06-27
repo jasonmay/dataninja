aba286ec192bb213e64bcc31327ef69b7fe0c10e
Jason May <jason.a.may@gmail.com>
2009-06-27 17:05:52 -0400

    "Commify" plugin for making numbers pretty

---
+++ lib/App/Dataninja/Bot/Plugin/Commify.pm
@@ -0,0 +1,43 @@
+package App::Dataninja::Bot::Plugin::Commify;
+use Moose;
+extends 'App::Dataninja::Bot::Plugin';
+
+use Number::Format qw(:subs);
+
+=head1 NAME
+
+App::Dataninja::Bot::Plugin::Commify - add commas to numbers
+
+=head1 COMMANDS
+
+=over
+
+=item * commify B<description>
+
+When a user gives dataninja a number using this command, dataninja outputs
+the number with commas put in the right places.
+
+=back
+
+=cut
+
+around 'command_setup' => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    $self->command(
+        commify => sub {
+            my $command_arg = shift;
+            my $output =  eval { format_number $command_arg };
+            return $@ if $@;
+            return $output;
+        }
+    );
+};
+
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
