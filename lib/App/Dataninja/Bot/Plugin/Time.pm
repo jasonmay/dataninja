feda85730e575d674eab116ac299d9384281d4cb
Jason May <jason.a.may@gmail.com>
2009-06-28 16:30:59 -0400

    new plugin: Time (for time zone time checking)

---
+++ lib/App/Dataninja/Bot/Plugin/Time.pm
@@ -0,0 +1,42 @@
+package App::Dataninja::Bot::Plugin::Time;
+use Moose;
+use DateTime;
+extends 'App::Dataninja::Bot::Plugin';
+
+=head1 NAME
+
+App::Dataninja::Bot::Plugin::Time - get the time for a certain time zone
+
+=head1 COMMANDS
+
+=over
+
+=item * time B<[time zone]>
+
+This plugin gets the time for a certain time zonek.
+
+=back
+
+=cut
+
+around 'command_setup' => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    $self->command(
+        time => sub {
+            my $command_args = shift;
+            return "please specify a timezone (Area/Location format)"
+                unless $command_args;
+            my $dt = eval { DateTime->now(time_zone => $command_args) };
+            return $@ if $@;
+            return sprintf("%s %s", $dt->ymd, $dt->hms);
+        }
+    );
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
