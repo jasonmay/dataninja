d483155a40deffef08aa4ae98c260e65b1d5e4a1
Jason May <jason.a.may@gmail.com>
2010-04-05 19:27:19 -0400

    RNG plugin

---
+++ lib/App/Dataninja/Bot/Plugin/Rng.pm
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+package App::Dataninja::Bot::Plugin::Rng;
+use Moose;
+use namespace::autoclean;
+extends 'App::Dataninja::Bot::Plugin';
+
+around 'command_setup' => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    $self->command(
+        rng => sub {
+            my $args = shift;
+            my @choices = split ' ', $args;
+            warn "test";
+
+            return $choices[rand @choices];
+        }
+    );
+};
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
